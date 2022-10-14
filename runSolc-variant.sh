6ab52f52b932108405d9d10e16f92f0daaac0ed8
loulankxh <178927469@qq.com>
2022-10-13 23:17:27 -0400

    experimenys_Results

--- runSolc-variant.sh
+++ runSolc-variant.sh
@@ -10,21 +10,22 @@ do
 	if [[ "$CONTRACT_NAME" == *".sol" ]]; 
 	then
 		echo $PATH_TO_CONTRACTS
+		output_file=${CONTRACT_NAME%.*}.txt
 		echo $CONTRACT_NAME
-		time docker run --rm -v $PATH_TO_CONTRACTS:/contracts solc-verify:latest /contracts/$CONTRACT_NAME
+		(time docker run --rm -v $PATH_TO_CONTRACTS:/contracts solc-verify:latest /contracts/$CONTRACT_NAME) >& ./output/solidity/${output_file}
 	fi
 done
 
 PATH_TO_CONTRACTS=${PROJECT_PATH}dsc-instrument
-#CONTRACT_NAME=auction.sol
 
 for CONTRACT_NAME in `ls $PATH_TO_CONTRACTS/`
 do
         if [[ "$CONTRACT_NAME" == *".sol" ]];
         then
 		echo $PATH_TO_CONTRACTS
+		output_file=${CONTRACT_NAME%.*}.txt
                 echo $CONTRACT_NAME
-                time docker run --rm -v $PATH_TO_CONTRACTS:/contracts solc-verify:latest /contracts/$CONTRACT_NAME
+                (time docker run --rm -v $PATH_TO_CONTRACTS:/contracts solc-verify:latest /contracts/$CONTRACT_NAME) >& ./output/dsc-instrument/${output_file}
         fi
 done
 
